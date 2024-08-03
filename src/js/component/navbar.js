9a3bae87b528592b98353f36089c1379f92aceff
Juan Carlos Díaz <169100845+Nxusbets@users.noreply.github.com>
2024-08-03 01:57:21 +0000

    navbar terminada

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -6,10 +6,11 @@ import { Navbar, Nav, NavItem } from 'react-bootstrap';
 const NavBar = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand href="#home">Start Bootstrap</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav " />
-      <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="justify-content-end" activeKey="/home">
+      <Navbar.Brand href="#home" className = "px-5">Start Bootstrap</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse className = "px-5 justify-content-end" id="basic-navbar-nav ">
+      <Nav activeKey="/home">
+      
         <Nav.Item>
           <Nav.Link href="#home">Home</Nav.Link>
         </Nav.Item>
@@ -17,8 +18,10 @@ const NavBar = () => {
           <Nav.Link href="#Services">Services</Nav.Link>
           <Nav.Link href="#Contact">Contact</Nav.Link>
         </Nav>
+        
       </Navbar.Collapse>
     </Navbar>
+    
   );
 };
 
